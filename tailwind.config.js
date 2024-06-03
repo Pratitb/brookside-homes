bdcbc221318b64fe5433e705f4a5e626538638fb
pratitb <pratit.bangdiwala7@gmail.com>
2024-06-03 17:58:10 +0530

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  content: ["./public/**/*.html"],
+  theme: {
+    fontFamily: {
+      sans: ["Rubik", "Helvetica", "Arial", "sans-serif"],
+    },
+
+    container:{
+      padding: '2rem',
+      center: true
+    },
+    extend: {},
+  },
+  plugins: [],
+}
